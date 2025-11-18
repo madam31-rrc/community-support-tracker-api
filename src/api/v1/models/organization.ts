d4bb92c8bf703978597e1b40bbbdb09138f163b4
madam31-rrc <madam31@academic.rrc.ca>
2025-11-17 20:23:30 -0600

    feat: made changes to Organization files

--- src/api/v1/models/organization.ts
+++ src/api/v1/models/organization.ts
@@ -1,19 +1,19 @@
 import Joi from 'joi';
 
-export type Organization = {
+export interface Organization {
   id: string;
   name: string;
   slug?: string;
-  createdAt: string; // ISO
+  createdAt: string;
   updatedAt?: string;
-};
+}
 
-export const createOrgSchema = Joi.object({
+export const createOrganizationSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  slug: Joi.string().alphanum().min(3).max(30).optional()
+  slug: Joi.string().alphanum().min(3).max(40).optional()
 });
 
-export const updateOrgSchema = Joi.object({
-  name: Joi.string().min(3).optional(),
-  slug: Joi.string().alphanum().min(3).max(30).optional()
+export const updateOrganizationSchema = Joi.object({
+  name: Joi.string().min(3),
+  slug: Joi.string().alphanum().min(3).max(40)
 }).min(1);
