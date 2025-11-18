d4a5da8fc6bf3d3295ac945b908315cb0ba6d569
madam31-rrc <madam31@academic.rrc.ca>
2025-11-17 21:19:01 -0600

    feat: update index.ts to conain CRUD and some extras

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import organizationRoutes from './organization.routes';
+import volunteerRoutes from './volunteer.routes';
 
 const router = Router();
 
 router.use('/organizations', organizationRoutes);
+router.use('/volunteers', volunteerRoutes);
 
 export default router;
